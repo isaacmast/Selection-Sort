cbc35604d1ca844657dcd948455d41e808ca0e3d
Isaac Mast <isaac.mast@emu.edu>
2015-11-07 18:19:50 -0500

    Added count variable for counting basic operation executions

--- selectionsort.py
+++ selectionsort.py
@@ -4,7 +4,8 @@ import random, string, timeit
 Determines the execution time of a selectionsort algorithm, given an 
 array of random strings. I do not own some of this code. All rigths 
 and original code of the wrapper and wrapped functions belong to 
-Xiaonuo Gantan from Python Central. 
+Xiaonuo Gantan from Python Central at 
+pythoncentral.io/time-a-python-function/. 
 '''
 
 def wrapper(func, *args, **kwargs):
@@ -22,19 +23,23 @@ def selectionsort(array):
     Input: An array of strings, array
     Output: The sorted array of strings
     '''
+	count = 0
     for i in range(len(array)):
 		minPos = i
 		for j in range(i, len(array)):
+			count += 1
 			if array[j] < array[minPos]:
 				minPos = j
 		temp = array[i]
 		array[i] = array[minPos]
 		array[minPos] = temp
+	return count
 
 array = []
-arrayLength = 10
-stringLength = 10
-for i in range(0, arrayLength):
-	array.append(''.join(random.choice(string.lowercase) for i in range(stringLength)))
+ARRAYLENGTH = 1000
+STRINGLENGTH = 10
+for i in range(0, ARRAYLENGTH):
+	array.append(''.join(random.choice(string.lowercase) for i in range(STRINGLENGTH)))
 wrapped = wrapper(selectionsort, array)
+print selectionsort(array)
 print timeit.timeit(wrapped, number = 1000)
