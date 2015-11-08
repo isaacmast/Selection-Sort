f2922a1b59df883317888dc4a42f26123a762130
Isaac Mast <isaac.mast@emu.edu>
2015-11-08 02:50:23 -0500

    Added comments

--- selectionsort.py
+++ selectionsort.py
@@ -2,29 +2,28 @@ import random, string, timeit
 
 '''
 Determines the execution time of a selectionsort algorithm, given an 
-array of random strings. I do not own some of this code. All rigths 
-and original code of the wrapper and wrapped functions belong to 
-Xiaonuo Gantan from Python Central at 
-pythoncentral.io/time-a-python-function/. 
+array of random strings. All rigths and original code of the wrapper 
+and wrapped functions belong to Xiaonuo Gantan from Python Central at 
+"pythoncentral.io/time-a-python-function/". 
 '''
 
 def wrapper(func, *args, **kwargs):
-    '''
-    Wrapper function to wrap selectionsort function in to determine
-    the execution time of the algorithm.
-    '''
-    def wrapped():
-        return func(*args, **kwargs)
-    return wrapped
+	'''
+	Wrapper function to wrap selectionsort function in to determine
+	the execution time of the algorithm.
+	'''
+	def wrapped():
+		return func(*args, **kwargs)
+	return wrapped
 
 def selectionsort(array):
-    '''
-    Selectionsort algorithm for sorting an array of strings
-    Input: An array of strings, array
-    Output: The sorted array of strings
-    '''
+	'''
+	Selectionsort algorithm for sorting an array of strings
+	Input: An array of strings, array
+	Output: The sorted array of strings
+	'''
 	count = 0
-    for i in range(len(array)):
+	for i in range(len(array)):
 		minPos = i
 		for j in range(i, len(array)):
 			count += 1
@@ -35,11 +34,21 @@ def selectionsort(array):
 		array[minPos] = temp
 	return count
 
+#Assign variables
 array = []
-ARRAYLENGTH = 1000
+ARRAYLENGTH = 2000
 STRINGLENGTH = 10
+
+#Generate array, length ARRAYLENGTH, of random strings, 
+#length STRINGLENGTH
 for i in range(0, ARRAYLENGTH):
 	array.append(''.join(random.choice(string.lowercase) for i in range(STRINGLENGTH)))
+
+#Wrap algorithm to determine execution time
 wrapped = wrapper(selectionsort, array)
+
+#Determine basic operation execution count
 print selectionsort(array)
-print timeit.timeit(wrapped, number = 1000)
+
+#Determine execution time of algorithm
+print (timeit.timeit(wrapped, number = 1000)) / 1000
